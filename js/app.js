9a6a9c57351db86d61c29c89757e4bbc56fbec17
NazarSak <112251832+NazarSak@users.noreply.github.com>
2023-10-30 21:44:59 +0200

    try

--- js/app.js
+++ js/app.js
@@ -45,3 +45,31 @@ createObserver('.projectsFragment3', 'animation', 'left')
 createObserver('.projectsFragment4', 'animation', 'right')
 createObserver('.servicesCards', 'animation', 'bottom')
 createObserver('.header', 'animation', 'top')
+
+function smoothScroll(target, duration) {
+    var targetElement = document.querySelector(target);
+    var targetPosition = targetElement.getBoundingClientRect().top;
+    var startPosition = window.pageYOffset;
+    var startTime = null;
+
+    function animation(currentTime) {
+        if (startTime === null) startTime = currentTime;
+        var timeElapsed = currentTime - startTime;
+        var progress = timeElapsed / duration;
+
+        window.scrollTo(0, easeInOutCubic(progress, startPosition, targetPosition, duration));
+
+        if (timeElapsed < duration) {
+            requestAnimationFrame(animation);
+        }
+    }
+
+    function easeInOutCubic(t, b, c, d) {
+        t /= d / 2;
+        if (t < 1) return c / 2 * t * t * t + b;
+        t -= 2;
+        return c / 2 * (t * t * t + 2) + b;
+    }
+
+    requestAnimationFrame(animation);
+}
